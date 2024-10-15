46b507ec024d735dd4b66d40fc8d56c8f05f58af
ferrazbenedito <123019011+ferrazbenedito@users.noreply.github.com>
2024-10-15 11:10:39 -0300

    Update sttest2.py

--- sttest2.py
+++ sttest2.py
@@ -12,13 +12,29 @@ with st.container():
 
 @st.cache_data
 def carregar_dados():
-    tabela = pd.read_csv("resultados.csv")
+    tabela = pd.read_excel("Merged_4.xlsx")
     return tabela
 
 with st.container():
+    st.subheader("Vamos Começar Com Algumas Perguntas Básicas")
+
+    st.title("Ao Final Desse Programa Voçê Vai Poder Saber Todas As Infomações Econômicas E Financeiras Do Seu Empreendimento!")
+
+    st.write("E Melhor, Tera Um Dashboard Completo Com Todas As Avaliações \n [Ou se quiser pode saber mais sobre o melhor empreendimento de Alta Floresta](https://jardimsantacecilia.com.br/) ")
+
+with st.container():
     st.write("---")
-    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
-    num_dias = int(qtde_dias.replace("D", ""))
-    dados = carregar_dados()
-    dados = dados[-num_dias:]
-    st.area_chart(dados, x="Data", y="Contratos")
+
+    st.area_chart(data= df, x= "key", y= ["df_sum_columns_Entrada",	"df_sum_columns_Parcela",	"df_sum_columns_Comissao"	,"Receita_Distribuivel",	"df_Pos_Obra_Mensal",	"df_Pre_Obra_Mensal"])
+
+
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+st.write("Outside the form")
